e281d4d492f636c56640f256f9c7e531decfb104
Sash Zats <sash@zats.io>
2015-01-13 12:42:34 +0200

    Added UICollectionView convenience methods

---
+++ Pod/Classes/UIKit+Doppelganger.h
@@ -0,0 +1,15 @@
+//
+//  UIKit+Doppelganger.h
+//  Pods
+//
+//  Created by Sash Zats on 1/13/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionView (Doppelganger)
+
+- (void)wml_applyBatchChanges:(NSArray *)changes inSection:(NSUInteger)section completion:(void (^)(BOOL finished))completion;
+
+@end
---
+++ Pod/Classes/UIKit+Doppelganger.m
@@ -0,0 +1,51 @@
+//
+//  UIKit+Doppelganger.m
+//  Pods
+//
+//  Created by Sash Zats on 1/13/15.
+//
+//
+
+#import "UIKit+Doppelganger.h"
+
+#import "WMLArrayDiff.h"
+
+
+static NSString *const WMLArrayDiffSourceIndexPathKey = @"WMLArrayDiffSourceIndexPathKey";
+static NSString *const WMLArrayDiffDestinationIndexPathKey = @"WMLArrayDiffDestinationIndexPathKey";
+
+
+@implementation UICollectionView (Doppelganger)
+
+- (void)wml_applyBatchChanges:(NSArray *)changes inSection:(NSUInteger)section completion:(void (^)(BOOL))completion {
+    NSMutableArray *insertion = [NSMutableArray array];
+    NSMutableArray *deletion = [NSMutableArray array];
+    NSMutableArray *moving = [NSMutableArray array];
+    
+    for (WMLArrayDiff *diff in changes) {
+        switch (diff.type) {
+            case WMLArrayDiffTypeDelete:
+                [deletion addObject:[NSIndexPath indexPathForItem:diff.previousIndex inSection:section]];
+                break;
+            
+            case WMLArrayDiffTypeInsert:
+                [insertion addObject:[NSIndexPath indexPathForItem:diff.currentIndex inSection:section]];
+                break;
+
+            case WMLArrayDiffTypeMove:
+                [moving addObject:diff];
+                break;
+        }
+    }
+    
+    [self performBatchUpdates:^{
+        [self insertItemsAtIndexPaths:insertion];
+        [self deleteItemsAtIndexPaths:deletion];
+        for (WMLArrayDiff *diff in moving) {
+            [self moveItemAtIndexPath:[NSIndexPath indexPathForItem:diff.previousIndex inSection:section]
+                          toIndexPath:[NSIndexPath indexPathForItem:diff.currentIndex inSection:section]];
+        }
+    } completion:completion];
+}
+
+@end
